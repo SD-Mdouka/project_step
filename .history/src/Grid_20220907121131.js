966cdc8955f4b63cd364c887f4b25c6e411e7f3b
SD-Mdouka <sd.mdouka@gmail.com>
2022-09-07 17:37:55 +0100

    first commit

---
+++ .history/src/Grid_20220907121131.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style-grid.css";
+
+export const Grid = () => {
+  return (
+    <div>
+      <div className="containt">
+        <div className="header">header</div>
+        <div className="box-1">box-1</div>
+        <div className="box-2">box-2</div>
+        <div className="box-3">box-3</div>
+        <div className="sidbar">sidbar</div>
+        <div className="content">content</div>
+        <div className="footer">footer</div>
+      </div>
+    </div>
+  );
+};
---
+++ .history/src/Grid_20220907125232.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style-grid.css";
+
+const Grid = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="header">header</div>
+        <div className="box-1">box-1</div>
+        <div className="box-2">box-2</div>
+        <div className="box-3">box-3</div>
+        <div className="sidebar">sidbar</div>
+        <div className="content">content</div>
+        <div className="footer">footer</div>
+      </div>
+    </div>
+  );
+};
+export default Grid;
